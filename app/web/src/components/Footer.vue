780a345db9c98bb6364859dac95741bc9c5af2be
Mario Vargas <mvargas@cardinalsolutions.com>
2018-06-27 15:25:23 +0000

    Add extra exclamation point

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Mario Vargas CI/CD!!
+        Mario Vargas CI/CD!!!
       </div>
       <div class="col-lg-6">
       </div>
