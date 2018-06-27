7c8c5d49e3ec18c50b07ea90c0f7b7c6c4183fab
Mario Vargas <mvargas@cardinalsolutions.com>
2018-06-27 16:16:42 +0000

    Change developer name

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Mario Vargas CI/CD!!!
+        Developer Name... Ha ha ha.
       </div>
       <div class="col-lg-6">
       </div>
