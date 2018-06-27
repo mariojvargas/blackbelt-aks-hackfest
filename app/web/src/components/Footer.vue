edebd74edf348249e62b48e807127fa5710d0c1b
Mario Vargas <mvargas@cardinalsolutions.com>
2018-06-27 15:02:05 +0000

    Add manifests and Dockerfile

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Global Blackbelt Team
+        Mario Vargas CI/CD!!
       </div>
       <div class="col-lg-6">
       </div>
