ce2b9e53a711389fd3c7198469c96ac3c4f64e79
Mario Vargas <mvargas@cardinalsolutions.com>
2018-06-27 16:23:10 +0000

    Use real developer name

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Developer Name... Ha ha ha.
+        Mario Vargas @ Cardinal Solutions
       </div>
       <div class="col-lg-6">
       </div>
